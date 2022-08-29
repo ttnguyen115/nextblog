76f57a19655588c4c3850dce1b147ae62935b859
Jed <thanhtrung1012000@gmail.com>
2022-08-29 22:19:02 +0700

    feat: setup and override theme MUI for layout

--- components/layout/MainLayout/index.tsx
+++ components/layout/MainLayout/index.tsx
@@ -1,12 +1,13 @@
 import { Footer, Header } from '@/components/common';
 import { LayoutProps } from '@/types/common';
-import { Box, Stack } from '@mui/material';
+import { Box, Container, Stack } from '@mui/material';
 import Link from 'next/link';
 
 export function MainLayout({ children }: LayoutProps) {
 	return (
 		<Stack minHeight="100vh">
 			<Header />
+
 			<Link href="/">
 				<a>Home</a>
 			</Link>
@@ -17,6 +18,10 @@ export function MainLayout({ children }: LayoutProps) {
 				<a>Works</a>
 			</Link>
 			<Box component="main" flexGrow={1}>
+				<Container maxWidth="sm" sx={{ bgcolor: 'primary.main' }}>
+					SM Container
+				</Container>
+				<Container sx={{ bgcolor: 'primary.main' }}>SM Container</Container>
 				{children}
 			</Box>
 			<Footer />

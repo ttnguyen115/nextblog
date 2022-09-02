43a96e2783b3b214ad68d1c07dd0eba347b3c974
Jed <thanhtrung1012000@gmail.com>
2022-09-02 11:55:01 +0700

    feat: add footer page

--- components/common/Footer/index.tsx
+++ components/common/Footer/index.tsx
@@ -1,9 +1,49 @@
-import { Box } from '@mui/material';
+import { Facebook, GitHub, Instagram, LinkedIn } from '@mui/icons-material';
+import { Box, Icon, Stack, Typography } from '@mui/material';
 
 export function Footer() {
+	const socialLinks = [
+		{
+			id: '1',
+			icon: Facebook,
+			url: 'https://google.com',
+		},
+		{
+			id: '2',
+			icon: Instagram,
+			url: 'https://google.com',
+		},
+		{
+			id: '3',
+			icon: GitHub,
+			url: 'https://google.com',
+		},
+		{
+			id: '4',
+			icon: LinkedIn,
+			url: 'https://google.com',
+		},
+	];
+
 	return (
 		<Box component="footer" py={2} textAlign="center">
-			Footer
+			<Stack direction="row" justifyContent="center">
+				{socialLinks.map((item) => (
+					<Box
+						component="a"
+						key={item.id}
+						p={2}
+						href={item.url}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Icon component={item.icon} sx={{ fontSize: 48 }} />
+					</Box>
+				))}
+			</Stack>
+			<Typography>
+				Copyright ©{new Date().getFullYear()} All rights reserved
+			</Typography>
 		</Box>
 	);
 }
